6d840cd571d46d1b13bea870797f5c2d7debfeba
YounessIdl <younessidlamine@outlook.com>
2021-09-18 23:55:29 +0200

    #5

--- API/Pictyapi.js
+++ API/Pictyapi.js
@@ -1,10 +1,13 @@
-
 export function getEventsById(uId){
     const url = 'https://pictytest.herokuapp.com/eventsr/' + uId
-    return fetch(
-        url,{
-            method:'GET'
-        }) 
+    return fetch(url) 
+    .then((response) => response.json()) 
+    .catch((error) => console.log(error))
+}
+
+export function findEventFromEventID(eId){
+    const url = 'https://pictytest.herokuapp.com/events/' + eId
+    return fetch(url) 
     .then((response) => response.json()) 
     .catch((error) => console.log(error))
 }
